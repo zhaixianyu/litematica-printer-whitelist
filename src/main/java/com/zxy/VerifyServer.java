1f41e2242e174e0abff217350f17a87b842eeeab
zxy <1141927681@qq.com>
2023-03-31 15:27:28 +0800

    1.0

--- src/main/java/com/zxy/VerifyServer.java
+++ src/main/java/com/zxy/VerifyServer.java
@@ -3,42 +3,50 @@ package com.zxy;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class VerifyServer {
     public static VerifyServer ver;
     public static VerifyServer getVer(){
         return ver;
     }
+    public static boolean run  = true;
+    public static ServerSocket ssoc;
+    static {
+        try {
+            ssoc = new ServerSocket(25665);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void startVerify(){
         try {
-            ServerSocket ssoc = new ServerSocket(25665);
             File file = new File(System.getProperty("user.dir"));
             file = new File(file + "\\printerWhitelist.txt");
             file.createNewFile();
+            File finalFile = file;
             new Thread(() -> {
-                while(true){
+                while(run){
                     try {
                         Socket soc = ssoc.accept();
                         new Thread(()->{
                             try {
-                                BufferedReader whitelist = new BufferedReader(new FileReader("printerWhitelist.txt"));
-                                BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-                                if("投影打印机使用请求, 玩家ID: UUID:".equals(br.readLine())){
+                                BufferedReader whitelist = new BufferedReader(new FileReader(finalFile));
+                                BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream(), StandardCharsets.UTF_8));
+                                if("printer usage request,ID: UUID: ".equals(br.readLine())){
                                     String id = br.readLine();
                                     String uid = br.readLine();
-                                    System.out.println("玩家"+id+"正在请求使用投影打印机"+"uuid: " +uid);
+                                    System.out.println("[PrinterWhitelist]: "+id+" Request to use a printer"+"uuid: " +uid);
                                     String listid;
-                                    String yz = "拒绝";
+                                    String yz = whitelist.readLine();
                                     while((listid = whitelist.readLine())!=null){
                                         if(listid.equals(id) || listid.equals(uid)){
-                                            yz = "允许";
+                                            yz = "Y";
+                                            System.out.println("[PrinterWhitelist]: Allowed");
                                             break;
-                                        } else {
-                                            yz = "拒绝";
                                         }
                                     }
-                                    System.out.println("已"+yz);
-                                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+                                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream(),StandardCharsets.UTF_8));
                                     bw.write(yz);
                                     bw.newLine();
                                     bw.flush();
@@ -49,7 +57,6 @@ public class VerifyServer {
                                     soc.close();
                                 }
                             } catch (IOException e) {
-                                System.out.println("fileThrows");
                                 e.printStackTrace();
                             }
                         }).start();
@@ -59,7 +66,6 @@ public class VerifyServer {
                 }
             }).start();
         } catch (IOException e) {
-
             e.printStackTrace();
         }
     }
--- src/main/java/com/zxy/mixin/ExampleMixin.java
+++ src/main/java/com/zxy/mixin/ExampleMixin.java
@@ -1,11 +1,14 @@
 package com.zxy.mixin;
 
+
 import com.zxy.VerifyServer;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.IOException;
 
 
 @Mixin(MinecraftServer.class)
@@ -17,5 +20,14 @@ public class ExampleMixin {
 			return;
 		}
 		VerifyServer.getVer().startVerify();
+	}
+	@Inject(at = @At("HEAD"),method = "stop")
+	public void exit(CallbackInfo ci){
+		try {
+			VerifyServer.run = false;
+			VerifyServer.ssoc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
